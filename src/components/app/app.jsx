d19e14a22fce9d5c124667764d3472f663bed046
Roman Aleksyutin <gyxman@yandex.ru>
2020-02-20 20:59:35 +0300

    fix build

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Main} from "../main/Main.jsx";
+import {Main} from "../main/main.jsx";
 
 export const App = ({offers}) => {
   return <Main offers={offers}/>;
